1883a51219b7cc3c275fcf8188639fe1412919bd
MORONGWA <104618822+moronngwa@users.noreply.github.com>
2022-11-30 09:01:06 +0200

    Add project files.

---
+++ Objectorientatedprinciples/People.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objectorientatedprinciples
+{
+    class People
+    {
+        public string name;
+        public int Age;
+        public string Race;
+        public string Gender;
+
+
+        public People(string PersonName, string PersonGender, string PersonRace, int PersonAge)
+        {
+            name = PersonName;
+            Age = PersonAge;
+            Race = PersonRace;
+            Gender = PersonGender;
+        }
+    }
+}
---
+++ Objectorientatedprinciples/Program.cs
@@ -0,0 +1,19 @@
+﻿using Objectorientatedprinciples;
+
+class Program { 
+
+    static void Main(String[] args)
+    {
+        People Person1= new People("Malesela","Male","Black",27);
+        People Person2 = new People("Thabo","Female","Black",19);
+        People Person3 = new People("Martha","Female","Black",23);
+
+        Console.WriteLine(Person1.name+ " " + Person1.Gender +" " + Person1.Race +" " + Person1.Age);
+        Console.WriteLine(Person2.name + " " + Person2.Gender + " " + Person2.Race + " " + Person2.Age);
+        Console.WriteLine(Person3.name + " " + Person3.Gender + " " + Person3.Race + " " + Person3.Age);
+
+    }
+
+
+}
+
